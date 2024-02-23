fa7a5056af4ba8b6b3c44384c4544e5840167e6a
kohei <kouhei.stifler@gmail.com>
2024-02-23 16:21:14 +0900

    commit

--- FormSample/FormProject/FormApp/forms.py
+++ FormSample/FormProject/FormApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core import validators
-from .models import Post
+from .models import Post,ModelSetPost,User
 
 def check_name(value):
     if value == 'ああああ':
@@ -51,9 +51,66 @@ class UserInfo(forms.Form):
         verify_mail = cleaned_data['verify_mail']
         if mail != verify_mail:
             raise forms.ValidationError('メールアドレスが一致しません！！')
+        
+class BaseForm(forms.ModelForm):
+    def save(self,*args,**kwargs):
+        print(f'Form:{self.__class__.__name__}実行')
+        return super(BaseForm,self).save(*args,**kwargs)
 
-class PostModelForm(forms.ModelForm):
+class PostModelForm(BaseForm):
+    name = forms.CharField(label='名前')
+    title = forms.CharField(label='タイトル')
+    memo = forms.CharField(label='メモ')
+    memo = forms.CharField(
+        widget = forms.Textarea(attrs={'rows':30,'cols':20})
+    )
     
     class Meta:
         model = Post
+        fields = '__all__'
+        # fields = ['name','title']
+        # exclude =['title']
+
+    def save(self,*args,**kwargs):
+        obj = super(PostModelForm,self).save(commit=False,*args,**kwargs)
+        obj.name = obj.name.upper()
+        print(type(obj))
+        print('save実行')
+        obj.save()
+        return obj
+    
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if name == 'ああああ':
+            raise validators.ValidationError('名前が登録できません')
+        return name
+    
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if title == 'ああああ':
+            raise validators.ValidationError('そのタイトルは登録できません')
+        return title
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        is_exists = Post.objects.filter(title=title).first()
+        if is_exists:
+            raise validators.ValidationError('そのタイトルは既に存在します。')
+
+class FormSetPost(forms.Form):
+    title = forms.CharField(label='タイトル')
+    memo = forms.CharField(label='メモ')
+
+class ModelFormSetPost(forms.ModelForm):
+    title = forms.CharField(label='タイトル')
+    memo = forms.CharField(label='メモ')
+
+    class Meta:
+        model = ModelSetPost
+        fields = '__all__'
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
         fields = '__all__'
--- FormSample/FormProject/FormApp/models.py
+++ FormSample/FormProject/FormApp/models.py
@@ -5,3 +5,12 @@ class Post(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=255)
     memo = models.CharField(max_length=255)
+
+class ModelSetPost(models.Model):
+    title = models.CharField(max_length=255)
+    memo = models.CharField(max_length=255)
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    picture = models.FileField(upload_to='picture/%Y/%m/%d')
