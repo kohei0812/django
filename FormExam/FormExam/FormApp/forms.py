4456bc8a875334da945b0b47b8447d9a0f792ac9
kohei <kouhei.stifler@gmail.com>
2024-05-21 16:21:39 +0900

    aaa

---
+++ FormExam/FormExam/FormApp/forms.py
@@ -0,0 +1,13 @@
+from .models import Students
+from django import forms
+
+class StudentInsertForm(forms.ModelsForm):
+    
+    name = forms.CharField(label='名前')
+    age = forms.IntegerField(label='年齢')
+    grade = forms.IntegerField(label='学年')
+    picture = forms.FileField(label='ファイルアップロード')
+    
+    class Meta:
+        model = Students
+        fields = '__all__'
